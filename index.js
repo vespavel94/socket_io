42a485f085d78724e9d4c01010159af4ef8389bb
Unknown <ves.pavel94@gmail.com>
2019-09-05 16:44:51 +0300

    admin messages

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var connections = []
 var messages = []
 var currUser = ''
 
-// -----------LOGIN--------------------------------------
+// -----------LOGIN-AJAX-------------------------------------
 
 app.get('/login', urlencodedParser, function (request, response) {
   response.sendFile(__dirname + '/login.html')
@@ -21,7 +21,7 @@ app.post('/login', urlencodedParser, function(request, response) {
   response.send('got username')
 })
 
-//----------------MAIN-----------------------------------------------
+//----------------MAIN-AJAX----------------------------------------------
 
 app.get('/', urlencodedParser, function (request, response) {
   response.sendFile(__dirname + '/index.html')
@@ -32,28 +32,55 @@ app.post('/getname', urlencodedParser, function (request, response){
   response.send({ name: currUser })
 })
 
+//-------------------ADMIN-AJAX-----------------------------------------
+
+app.get('/admin', urlencodedParser, function(request, response) {
+  response.sendFile(__dirname + '/admin.html')
+})
+
+app.get('/admindata', urlencodedParser, function(request, response) {
+  response.send({ users: users })
+})
+
+//--------------SOCKETS-----------------------------------------------
+
 io.sockets.on('connection', function(socket) {
-  io.sockets.emit('render messages', messages)
-  console.log('connected succesfully')
   connections.push(socket)
-  
-  socket.on('disconnect', function(data) {
-    connections.splice(connections.indexOf(socket), 1)
-    console.log('user Disconnected')
+  socket.on('join', function(data) {
+    if (!users.includes(data.username)) {
+      users.push(data.username)
+    }
+    socket.join(data.username)
+    console.log(`User ${data.username} connected`)
   })
 
-  socket.on('send mess', function(data) {
-    messages.push(data)
-    io.sockets.emit('add mess', {
-      msg: data
-    })
-  })
-
-  socket.on('reset chat', function() {
-    messages = []
-    io.sockets.emit('reset')
-    console.log(messages)
+  socket.on('adminMessage', function(data){
+    io.sockets.in(data.reciever).emit('admin says', { message: data.message })
   })
 })
+
+// io.sockets.on('connection', function(socket) {
+//   io.sockets.emit('render messages', messages)
+//   console.log('connected succesfully')
+//   connections.push(socket)
+  
+//   socket.on('disconnect', function(data) {
+//     connections.splice(connections.indexOf(socket), 1)
+//     console.log('user Disconnected')
+//   })
+
+//   socket.on('send mess', function(data) {
+//     messages.push(data)
+//     io.sockets.emit('add mess', {
+//       msg: data
+//     })
+//   })
+
+//   socket.on('reset chat', function() {
+//     messages = []
+//     io.sockets.emit('reset')
+//     console.log(messages)
+//   })
+// })
 
 server.listen(3030)
